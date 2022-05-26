2fcba34f2be94225314a10b4026d5e6326df592d
Jonathan Tran <digitalsigna@gmail.com>
2022-05-25 23:14:34 -0700

    need to deploy to heroku, can install and use editor with manifest and service worker, with functionality of unfocus saving and usage without internet connection

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,58 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Webpack plugin to generate html and inject bundle
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Notes or Code Snippets'
+      }),
+
+      // Inject custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      // Create manifest.json file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Notes or Code Snippets',
+        short_name: 'Notes',
+        description: 'Notes or Code Snippets',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        // default CSS loader
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // Babel-loader in order to use ES6.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
